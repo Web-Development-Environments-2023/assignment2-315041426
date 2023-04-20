17a1211043ccc7980407ac37dc1f9e862a251616
Gil Michalovich <gilmich@post.bgu.ac.il>
2023-04-20 12:57:04 +0300

    implemented switching screend on js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// Code for dynamically loading content based on navigation bar clicks
-const contentDiv = document.getElementById("content");
-const navLinks = document.querySelectorAll("nav ul li a");
+const navLinks = document.querySelectorAll('.nav-link');
+const contentSections = document.querySelectorAll('.content-section');
 
-navLinks.forEach(link => {
-	link.addEventListener("click", e => {
-		e.preventDefault();
-		const page = link.getAttribute("href").substr(1) + ".html";
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function (e) {
+        e.preventDefault();
 
-		fetch(page)
-			.then(response => response.text())
-			.then(html
+        // Remove the active class from all content sections
+        for (let j = 0; j < contentSections.length; j++) {
+            contentSections[j].classList.remove('active');
+        }
+
+        // Add the active class to the selected content section
+        const targetId = this.getAttribute('data-target');
+        const targetSection = document.getElementById(targetId);
+        targetSection.classList.add('active');
+    });
+}

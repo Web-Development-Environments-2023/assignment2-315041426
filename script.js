da1370ac9f99f66340b88cb03f54bf8329030214
Gil Michalovich <gilmich@post.bgu.ac.il>
2023-04-23 15:40:09 +0300

    implemented the register form
    without the array of known users in the memory

--- script.js
+++ script.js
@@ -16,3 +16,107 @@ for (let i = 0; i < navLinks.length; i++) {
         targetSection.classList.add('active');
     });
 }
+
+/******************************************  Register Form Validation *********************************/
+
+                                    //check if password meets the requirement // 
+
+const passwordInput = document.getElementById("password");
+const passwordValidation = document.getElementById("password_validation");
+var isPassGood = false;
+var password = null;
+passwordInput.addEventListener("keyup", () => {
+     password = passwordInput.value;
+
+    // Check if password meets requirements
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /[0-9]/.test(password);
+    const isLongEnough = password.length >= 8;
+
+    // Update validation message
+    if (isLongEnough && (hasUpperCase || hasLowerCase) && hasNumbers) {
+        isPassGood = true;
+        passwordValidation.innerHTML = "Password is valid :)";
+    } else {
+        isPassGood = false;
+        passwordValidation.innerHTML = "Password must be at least 8 characters long and contain letters and numbers";
+    }
+});
+
+                                    // passwords matching check
+
+const password2Input = document.getElementById("password2");
+const passwordsMatch = document.getElementById("passwords-match");
+var isPassMatch = false;
+
+password2Input.addEventListener("keyup", () => {
+    const password2 = password2Input.value;
+
+    // Update validation message
+    if (password2 === password ) {
+        passwordsMatch.innerHTML = "Passwords match :)";
+        isPassMatch = true;
+    } else {
+        isPassMatch = false;
+        passwordsMatch.innerHTML = "password doesnt match!";
+    }
+});
+
+                            //first name and last name doesnt contain numbers
+
+const firstnameInput = document.getElementById("firstname");
+const lastnameInput = document.getElementById("lastname");
+const nameMessage = document.getElementById("name-validation");
+var nameIsGood = true;
+firstnameInput.addEventListener("keyup", nameWithoutNumbers);
+lastnameInput.addEventListener("keyup", nameWithoutNumbers);
+
+function nameWithoutNumbers(event) {
+    const firsthasNumbers = /[0-9]/.test(firstnameInput.value);
+    const lasthasNumbers = /[0-9]/.test(lastnameInput.value);
+    if (firsthasNumbers || lasthasNumbers) {
+        nameIsGood = false;
+        nameMessage.innerHTML = "first and last name can't contain numbers!";
+    }
+    else {
+        nameIsGood = true;
+        nameMessage.innerHTML = "";
+    }
+}
+const emailInput = document.getElementById("email");
+const emailMessage = document.getElementById("email-validation");
+var emailIsGood = false;
+
+emailInput.addEventListener("input", validateEmail);
+
+function validateEmail(event) {
+    const email = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(email)) {
+        emailIsGood = true;
+        emailMessage.innerHTML = "";
+    } else {
+        emailIsGood = false;
+        emailMessage.innerHTML = "Please enter a valid email address";
+    }
+}
+
+function checkRegister() {
+    var inputs = document.querySelectorAll('input[required], select[required], textarea[required]');
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            alert('Please fill in all required fields.');
+            return false;
+        }
+    }
+    if (!(isPassGood && isPassMatch && nameIsGood && emailIsGood)) {
+        alert('one of the fields has not been filled out correctly.')
+        return false;
+    }
+
+    // Submit the form if all required fields are filled
+    document.getElementById('regform').submit();
+}
+
